149a2557004d2ece65d14d89e01bd01c32fe9d2a
罗建彪 <13826933394@163.com>
2018-08-02 19:57:32 +0800

    增加挂单的所有功能，细化数据的交互

--- src/components/page/pos.vue
+++ src/components/page/pos.vue
@@ -4,8 +4,8 @@
             <el-row>
                 <el-col :span="7" class="pos-order order-list order-list">
                     <!-- 将tape设置为border-card -->
-                    <el-tabs type="border-card">
-                        <el-tab-pane label="订单">
+                    <el-tabs type="border-card" v-model="activeName" @tab-click="handleClick">
+                        <el-tab-pane label="订单" name="order">
                             <!-- 表格，使用border属性带有表格，还有添加classname -->
                             <el-table :data="tableData" border style="width:100%" :row-class-name="tableRowClassName">
                                 <el-table-column prop="goodsName" label="商品">
@@ -26,12 +26,32 @@
                                 <p><small>总金额：</small>{{totalMoney}}元</p>
                             </div>
                         </el-tab-pane>
-                        <el-tab-pane label="挂单">挂单</el-tab-pane>
-                        <el-tab-pane label="外卖">外卖</el-tab-pane>
+                        <!-- 挂单 -->
+                        <el-tab-pane label="挂单" name="bill">
+                                <el-table :data="outBillData" border style="width:100%" :row-class-name="tableRowClassName">
+                                <el-table-column prop="goodsName" label="商品">
+                                </el-table-column>
+                                <el-table-column prop="price" label="金额" width="50%">
+                                </el-table-column>
+                                <el-table-column prop="count" label="数量" width="50%">
+                                </el-table-column>
+                                <el-table-column  label="操作" fixed="right">
+                                     <template slot-scope="scope">
+                                        <el-button type="text" size="mini" @click="deleteBillGoods(scope.row)" >删除</el-button>
+                                        <el-button type="text" @click="addBillGoods(scope.row)" >增加</el-button>
+                                    </template>
+                                </el-table-column>
+                            </el-table>
+                            <div class="total-box">
+                                <p><small>总数量：</small>{{billTotalCount}}</p>
+                                <p><small>总金额：</small>{{billTotalMoney}}元</p>
+                            </div>
+                        </el-tab-pane>
+                        <el-tab-pane label="外卖" name="takeOut">外卖</el-tab-pane>
                     </el-tabs>
                     
                     <div class="bottom-button">
-                        <el-button type="primary">挂单
+                        <el-button type="primary" @click="addOutBill">挂单
                             <i class="el-icon-goods el-icon--right"></i>
                         </el-button>
                         <el-button type="danger" @click="dialogVisible = true">清除
@@ -128,10 +148,14 @@ import Qs from 'qs'
 export default {
     data() {
         return {
+            activeName: 'order',//选中的tab
             dialogVisible: false,//控制提示框
             totalCount: 0,//总数量
             totalMoney:  0,//总价格
+            billTotalCount: 0,//挂单的数量
+            billTotalMoney: 0,//挂单的金钱
             tableData:[],//订单的数据
+            outBillData: [],//挂单的数据
             hotSetMeal: [],//热门套餐数据
             type0Foodlists: [],//主食
             type1Foodlists: [],//小吃
@@ -164,6 +188,10 @@ export default {
             })
     },
     methods: {
+        handleClick(tab,event) {
+             console.log(tab, event);
+             console.log(this.activeName);
+        },
         //添加类名
         tableRowClassName({row,rowIndex}){
             if(rowIndex %2 == 0){
@@ -218,15 +246,47 @@ export default {
             console.log(this.tableData);
             this.getAllMoney();
         },
-        //总计算（价格和数量）
+        //删除挂单中的商品
+        deleteBillGoods(goods){
+            if(goods.count > 1){
+                this.outBillData.forEach(o =>{
+                if(o.goodsId == goods.goodsId){
+                    o.count = o.count-1
+                }
+                });
+            }else if(goods.count == 1){
+                this.outBillData = this.outBillData.filter(o => o.goodsId != goods.goodsId);
+            }
+            //算账
+            this.getAllMoney();
+        },
+        //添加挂单的份数
+        addBillGoods(goods) {
+            this.outBillData.forEach(item => {
+                if(item.goodsId == goods.goodsId){
+                    item.count++;
+                }
+            })
+            //算钱
+            this.getAllMoney();
+        },
+        //订单总计算（价格和数量）
         getAllMoney(){
             //这里初始化所有的商品件数和价格，因为每次进来的tableData都是重新进行赋值的，如果不删除，会进行操作前的数据相加
             this.totalCount = 0;
             this.totalMoney = 0;
+            this.billTotalCount = 0;
+            this.billTotalMoney = 0;
             if(this.tableData){
                 this.tableData.forEach((element) => {
                     this.totalCount += element.count;
                     this.totalMoney = this.totalMoney+ (element.price*element.count)
+                })
+            }
+            if(this.outBillData){
+                this.outBillData.forEach((element) => {
+                    this.billTotalCount += element.count;
+                    this.billTotalMoney = this.billTotalMoney+ (element.price*element.count)
                 })
             }
         },
@@ -261,35 +321,113 @@ export default {
             return time;
         },
         //结账，把结账后的所有数据插入detail集合
+        //结账根据activeName(进入了哪个tab)来知道需要结账哪个的数据
         addSettleData() {
-            //给订单的数据添加日期
-            this.tableData.forEach(item => item['time'] = this.getLocalTime())
-            //判断是否已经清空了
+            //给订单和挂单的数据添加日期
             if(this.totalCount != 0){
-                //进行post请求，提交json数据
-                axios({
-                    url:'http://localhost:3000/settle_post',
-                    method:'post',
-                    data:JSON.stringify(this.tableData),
-                    //transformRequest 表示可以在这里进行修改post数据
-                    // transformRequest: [function(data){
-                    //     return data;
-                    // }],
-                    //请求头类型改为josn数据
-                    headers: {
-                        'Content-Type': 'application/json'
+                this.tableData.forEach(item => item['time'] = this.getLocalTime())
+            }
+            if(this.billTotalCount != 0){
+                this.outBillData.forEach(item => item['time'] = this.getLocalTime())
+            }
+            if(this.activeName == 'order'){
+                //判断是否已经有商品
+                if(this.totalCount != 0){
+                    //进行post请求，提交json数据
+                    axios({
+                        url:'http://localhost:3000/settle_post',
+                        method:'post',
+                        data:JSON.stringify(this.tableData),
+                        //transformRequest 表示可以在这里进行修改post数据
+                        // transformRequest: [function(data){
+                        //     return data;
+                        // }],
+                        //请求头类型改为josn数据
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    //清空所有数据
+                    this.tableData = [];
+                    this.totalCount = 0;
+                    this.totalMoney = 0;
+                    this.$message({
+                        message: '订单结账成功',
+                        type: 'success'
+                    });
+                }else{
+                    this.$message.error('请先键入商品，不能进行空结')
+                }
+            }else if(this.activeName == 'bill'){
+                if(this.outBillData != 0){
+                    axios({
+                        url:'http://localhost:3000/settle_post',
+                        method:'post',
+                        data:JSON.stringify(this.outBillData),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    this.outBillData = [];
+                    this.billTotalCount = 0;
+                    this.billTotalMoney = 0;
+                    this.$message({
+                        message: '挂单结账成功',
+                        type: 'success'
+                    });
+                }else{
+                    this.$message.error('请先键入商品，不能进行空结')
+                }
+            }            
+        },
+        //挂单。把当前订单的数据转给挂单，如果挂单有数据了，则提示不能挂单
+        addOutBill() {
+            if(this.totalCount == 0){
+                this.$message.error('请先键入商品才能进行挂单!')  
+            }else{
+                //挂单没数据的话，直接转过来即可
+                if(this.outBillData.length == 0){
+                    this.outBillData = this.tableData;
+                }else{
+                    //存放数组的对象
+                    let arr = {};
+                    //把有相同id的商品，添加份数
+                    for(let i = 0;i < this.tableData.length;i++){
+                        for(let t = 0;t < this.outBillData.length;t++){
+                            if(this.outBillData[t]['goodsId'] == this.tableData[i]['goodsId']){
+                                this.outBillData[t]['count']++;
+                                break;
+                            }
+                        }
                     }
-                });
-                //清空所有数据
+                    //去重
+                    this.tableData.forEach(item => this.outBillData.push(item));
+                    //这里翻转数组是因为，对象的唯一性去重会把前面相同的去掉。
+                    //而我需要的数据是在前面，所以需要翻转一下。
+                    this.outBillData.reverse();
+                    for(let i = 0;i < this.outBillData.length;i++){
+                        //利用对象的唯一性进行去重
+                        //现在的arr是一个用goodsid做属性的对象
+                        arr[this.outBillData[i]['goodsId']] = this.outBillData[i];
+                    }
+                    //把arr里面对象的属性值赋值给挂单数据
+                    this.outBillData = [];//先清空
+                    console.log(arr);
+                    for(let item in arr){
+                        this.outBillData.push(arr[item]);
+                    }
+                    
+                }
+                console.log(this.outBillData.length);
+                //清空订单的所有数据
                 this.tableData = [];
                 this.totalCount = 0;
                 this.totalMoney = 0;
+                this.getAllMoney();
                 this.$message({
-                    message: '结账成功',
+                    message: '挂单成功',
                     type: 'success'
-                });
-            }else{
-                this.$message.error('请先键入商品，不能进行空结')
+                })
             }
         }
     },
